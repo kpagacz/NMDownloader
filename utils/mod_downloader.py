1617890ec0002a07283e57075ee77fe10e9c6eb8
Konrad Pagacz <konrad.pagacz@gmail.com>
2019-03-16 09:42:21 +0100

    0.3.0

--- utils/mod_downloader.py
+++ utils/mod_downloader.py
@@ -33,10 +33,107 @@ class NexusInterface:
     This class is responsible for all interactions with Nexus Mods site.
 
     Args:
+        authenticator: an instance of Authenticator class with a loaded API key
+    """
+    def __init__(self,
+                 authenticator: authorization.Authenticator = None)
+        self.authenticator = authenticator
+
+    def authenticate(self) -> requests.Response:
+        """Function, which sends a validation request to the API.
+
+        Returns:
+            requests.Response with the response from the API
+        """
+        headers = {
+            "key" : self.authenticator.api_key,
+            "accept" : "application/json"
+        }
+
+        query = NexusQuery(query_type=requests.get,
+                           headers=headers)
+        response = query.query("users/validate.json")
+
+        if response.status_code != 200
+            return None
+        else:
+            return response
+
+
+class NexusQuery:
+    """Handles API queries to Nexus Mods.
+    This class is responsible for handling API queries to Nexus
+    Mods site.
+
+    Args:
+        query_type (request): a request function, like requests.get or requests.post
+        game: str object defining the game on Nexus mods site.
+            Can be: 'skyrim', etc.
+        *args: additional positional arguments to request function
+        **kwargs: additional keyword arguments sent to request function
+
+    Returns:
+        a parsed response object
+
+    Examples:
 
     """
     def __init__(self,
-                 authenticator: authorization.Authenticator = None,
-                 mod_list: list = None):
-        self.authenticator = authenticator
-        self.mod_importer = mod_importer
+                 query_type = None,
+                 params : dict = None,
+                 headers: dict = None):
+        self._query_type = query_type
+        self._params = params
+        self._headers = headers
+        self._base_url = "https://api.nexusmods.com/v1/"
+
+    def query(self,
+                   url: str,
+                   params: dict = None,
+                   headers: dict = None):
+        """Sends requests to Nexus API.
+        Args:
+            url: a full url sent to Nexus API via requests functions
+            params(optional): parameters to requests, default is declared during
+                the initialization of the class
+            headers(optional): headers to requests, default is declared during
+                the initialization of the class
+
+        Returns:
+            requests.Response object
+
+        Examples:
+
+
+        Raises:
+            requests.exceptions.ConnectionError
+            requests.exceptions.Timeour
+            requests.exceptions.TooManyRedirects
+            """
+        if params is None:
+            params = self._params
+        if headers is None:
+            headers = self._headers
+        url = self._base_url + url
+
+        try:
+            with self._query_type(url,
+                                  params = params,
+                                  headers = headers,
+                                  timeout = 0.5) as r:
+                return r
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error occured: {}.".format(e))
+            return requests.Response()
+        except requests.exceptions.Timeout as e:
+            print("Connection timed out: {}.".format(e))
+            return requests.Response()
+        except requests.exceptions.TooManyRedirects as e:
+            print("Too many redirections: {}".format(e))
+            return requests.Response()
+        except requests.exceptions.HTTPError as e:
+            print("An HTTP error occured: {}.".format(e))
+            return requests.Response()
+        except requests.exceptions.URLRequired as e:
+            print("Please specify the URL: {}.".format(e))
+            return requests.Response()
